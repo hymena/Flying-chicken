2994e1f3086d8489d2b1f64363d3b20115e67370
Ömer Faruk Uysal <61942901+hymena@users.noreply.github.com>
2022-05-09 16:07:20 +0300

    Add files via upload

---
+++ game/src/main/Enemy.java
@@ -0,0 +1,38 @@
+package main;
+
+public class Enemy {
+
+	private int x;//X coordinate of cats'
+	private int y;//Y coordinate of cats'
+	private int speed = 4; //The movement speed of cats in each iteration 
+	
+	
+	public Enemy(int x ,int y) { // Constructor
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() { //Getters
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setX(int x) {//Setters
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void move() { // Function to move cats
+		this.y -= speed;
+	}
+	
+	public void setSpeed(int speed) {//Speed changes when the level changes
+		this.speed = speed;
+	}
+}
